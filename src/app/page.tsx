802ff2af7c8d74148cecf2ef8ff6eee291644c07
newtondotcom <asphalt9fr@gmail.com>
2025-11-27 23:32:15 +0100

    style: new simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-50 via-white to-purple-50">
-      <main className="container mx-auto px-4 py-20 max-w-4xl">
-        <div className="text-center mb-12">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4">
-            Ecole Tres Directe
-          </h1>
-          <p className="text-muted-foreground text-lg">
-            Outils pour améliorer votre expérience avec EcoleDirecte
-          </p>
-        </div>
-        <button onClick={() => {
-          router.push("/login");
-        }}>
-          Connexion
-        </button>
-      </main>
+    <div className="h-screen rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
+      <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+        <span>Découvrez</span>
+        <span className="font-bold">Ecole Tres Directe</span>
+      </h2>
+      <Link href="/login">
+      <HoverBorderGradient
+        containerClassName="rounded-full mt-14 w-fit"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+      >
+        <span>Tester</span>
+        <Image src="/etd.svg" alt="Ecole Tres Directe" width={20} height={20} />
+      </HoverBorderGradient>
+      </Link>
+      <ShootingStars />
+      <StarsBackground />
     </div>
+
+    
   );
 }
