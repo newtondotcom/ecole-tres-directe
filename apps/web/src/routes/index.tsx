05c9ecae1ab4bbf21627de434a6509b939096a6d
newtondotcom <asphalt9fr@gmail.com>
2025-12-01 11:33:36 +0100

    style: fix landing page weird layout

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -9,7 +9,7 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
 	return (
-		<div className="container mx-auto max-w-3xl px-4 py-2">
+		<div className="w-full h-full">
 			<div className="h-screen rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
 			<h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
 				<span>Découvrez</span>
